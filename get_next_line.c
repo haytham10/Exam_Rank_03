61902eb2be03832020be0b4381cc98e6a5e019d8
haytham10 <cnarabi@gmail.com>
2022-07-06 22:24:34 +0100

    gnl2.0

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hmokhtar <hmokhtar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 19:55:21 by hmokhtar          #+#    #+#             */
-/*   Updated: 2022/07/06 22:22:55 by hmokhtar         ###   ########.fr       */
+/*   Updated: 2022/07/06 22:24:13 by hmokhtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,15 +31,15 @@ char *ft_strdup(char *s)
 
 char *get_next_line(int fd)
 {
-    static char    *buffer_reminder;
-    buffer_reminder = 0;
     if (fd < 0)
         return (NULL);
+	
     char buff[1];
     char line[1000000];
     line[0] = 0;
     int i = 0;
     int n;
+	
     while ((n = read(fd, buff, 1)) && n > 0)
     {
         line[i++] = buff[0];
